e639698a90b6b6427c1c63642c7d71809e81aafc
Jacob Caggese <jcaggese@gmail.com>
2022-04-13 14:58:00 -0400

    Added methods
    Refactored projects to utilize methods so JUnit can be integrated into these projects.

--- DragonCave/src/Main.java
+++ DragonCave/src/Main.java
@@ -2,25 +2,44 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("You are in a land full of dragons. In front of you, you see two caves." +
+                "In one cave, the dragon is friendly and will share his treasure with you." +
+                "The other dragon is greedy and hungry and will eat you in sight." +
+                "Which cave will you go into? (1 or 2)");
+        String response = fetchResponse();
+        System.out.println("You approach the cave..." +
+                "\nIt is dark and spooky..." +
+                "\nA large dragon jumps out in front of you! He opens his jaws and...");
+        System.out.println(generateOutcome(response));
+    }
+
+    /**
+     * @return User response, cave 1 or 2
+     */
+    private static String fetchResponse() {
+        String response = "1";
         try (Scanner in = new Scanner(System.in)) {
-            System.out.println("You are in a land full of dragons. In front of you, you see two caves." +
-                    "In one cave, the dragon is friendly and will share his treasure with you." +
-                    "The other dragon is greedy and hungry and will eat you in sight." +
-                    "Which cave will you go into? (1 or 2)");
-            String response = in.nextLine();
+            response = in.nextLine();
             while (!response.equals("1") && !response.equals("2")) {
                 System.out.println("There are only 2 caves to choose from! Please pick 1 or 2!");
                 response = in.nextLine();
             }
-                System.out.println("You approach the cave..." +
-                        "\nIt is dark and spooky..." +
-                        "\nA large dragon jumps out in front of you! He opens his jaws and...");
-            if (response.equals("1"))
-                System.out.println("Gobbles you down in one bite!");
-            else
-                System.out.println("Politely offers you some of his treasure.");
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("Uh oh! Error detected! Exiting game.");
+            System.exit(1);
         }
+        return response;
+    }
+
+    /**
+     * @param response User generated response, 1 or 2
+     * @return String representing win state.
+     */
+    private static String generateOutcome(String response) {
+        if (response.equals("1"))
+            return "Gobbles you down in one bite!";
+        else
+            return "Politely offers you some of his treasure.";
     }
 }
--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -10,51 +10,77 @@ public class Main {
             boolean play = true;
             while (play) {
                 System.out.printf("Well, %s, I am thinking of a number between 1 and 20. Take a guess.%n", user);
-                int num = rand.nextInt(20) + 1;
-                int i = 0;
-                boolean wrong = true;
-                while (i++ < 6 && wrong) {
-                    boolean valid = false;
-                    int guess = 0;
-                    while(!valid) { //enforce numeric input
-                        String input = in.nextLine();
-                        try {
-                            guess = Integer.parseInt(input);
-                            valid = true;
-                        } catch (NumberFormatException e) {
-                            System.out.println("Guess a number between 1 and 20!");
-                        }
-                    }
-                    if (guess < num) {
-                        System.out.println("Your guess is too low. Try again." +
-                                "\n You have " + (6 - i) + " guess(es) remaining.");
-                    } else if (guess > num) {
-                        System.out.println("Your guess is too high. Try again." +
-                                "\n You have " + (6 - i) + " guess(es) remaining.");
-                    } else {
-                        System.out.printf("Good job, %s, you got the number in %d guess(es)%n", user, i);
-                        wrong = false;
-                    }
-                }
-                if (wrong)
-                    System.out.printf("You failed to guess the number in 6 tries.%nThe number was: %d.%n", num);
-
+                playGame(in, user);
                 System.out.println("Would you like to play again? (Y/N)");
-                boolean accept = false;
-                while (!accept) {
-                    String again = in.nextLine();
-                    if (again.equals("Y") || again.equals("y")) {
-                        accept = true;
-                    }
-                    else if (again.equals("N") || again.equals("n")){
-                        accept = true;
-                        play = false;
-                        System.out.println("Thanks for playing.");
-                    }
-                }
+                play = playAgain(in);
             }
         } catch (Exception e) {
             e.printStackTrace();
+            System.out.println("Uh oh! Error detected! Exiting game.");
+        }
+    }
+
+    /**
+     * Allows user to input numbers below 1 and above 20 if they really want
+     * @param in Scanner to generate user input
+     * @return a number between 1 and 20
+     */
+    private static int getNumericInput(Scanner in) {
+        boolean valid = false;
+        int guess = -1;
+        while(!valid) { //enforce numeric input
+            String input = in.nextLine();
+            try {
+                guess = Integer.parseInt(input);
+                valid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("Guess a number between 1 and 20!");
+            }
+        }
+        return guess;
+    }
+
+    /**
+     * Main game loop
+     * @param in Scanner to get user response
+     * @param user User's name
+     */
+    private static void playGame(Scanner in, String user) {
+        int num = new Random().nextInt(20) + 1;
+        int i = 0;
+        boolean wrong = true;
+        while (i++ < 6 && wrong) {
+            int guess = getNumericInput(in);
+            if (guess < num) {
+                System.out.println("Your guess is too low. Try again." +
+                        "\n You have " + (6 - i) + " guess(es) remaining.");
+            } else if (guess > num) {
+                System.out.println("Your guess is too high. Try again." +
+                        "\n You have " + (6 - i) + " guess(es) remaining.");
+            } else {
+                System.out.printf("Good job, %s, you got the number in %d guess(es)%n", user, i);
+                wrong = false;
+            }
+        }
+        if (wrong)
+            System.out.printf("You failed to guess the number in 6 tries.%nThe number was: %d.%n", num);
+
+    }
+
+    /**
+     * @param in Scanner to get user input
+     * @return True if user wants to play again, False if not
+     */
+    private static boolean playAgain(Scanner in) {
+        while (true) {
+            String again = in.nextLine();
+            if (again.equals("Y") || again.equals("y")) {
+                return true;
+            }
+            else if (again.equals("N") || again.equals("n")){
+                System.out.println("Thanks for playing.");
+                return false;
+            }
         }
     }
 }
