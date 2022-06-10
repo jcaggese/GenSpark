75413d586cefa2d2150a547f162870f7d14fbb79
Jacob Caggese <jcaggese@gmail.com>
2022-06-10 17:09:13 -0400

    Portfolio Initial Commit
    Initial commit, need to add projects and enhance styling.

---
+++ WebDev/Portfolio/portfolio/src/App.js
@@ -0,0 +1,14 @@
+import Header from './Components/Header'
+import BioTile from './Components/BioTile'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <Header />
+      <BioTile />
+    </div>
+  );
+}
+
+export default App;
---
+++ WebDev/Portfolio/portfolio/src/Components/BioTile.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function BioTile() {
+    return (
+        <div className="bio-tile">
+            <img src="/images/Head.jpg"/>
+            <h3>Jacob Caggese</h3>
+            <p>Bio Here</p>
+            <ul>
+                <li><a>GitHub Link</a></li>
+                <li><a>LinkedIn Link</a></li>
+            </ul>
+        </div>
+    )
+}
