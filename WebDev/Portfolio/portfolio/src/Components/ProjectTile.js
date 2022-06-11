76eb7f4f2fe03b6960f67e7380f77ec9c280f53f
Jacob Caggese <jcaggese@gmail.com>
2022-06-11 17:47:17 -0400

    Added images to projects and made bio element fixed

--- WebDev/Portfolio/portfolio/src/Components/ProjectTile.js
+++ WebDev/Portfolio/portfolio/src/Components/ProjectTile.js
@@ -8,19 +8,13 @@ export default function ProjectTile() {
             <h2>Projects</h2>
             <div className="container">
                 <div className="row">
-                    <Project title="Humans Vs. Goblins" description="Basic Game created in Java with GUI created with JavaFX" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/GenSpark/tree/main/HumansVsGoblinsGUI"/>
-                    <Project title="Boarding Pass" description="Boarding Pass Form created in Java with JavaFX" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/Boarding-Pass"/>
-                    <Project title="Hangman Game" description="Hangman Game created using functional methods in Java" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/GenSpark/tree/main/HangmanFunctional"/>
+                    <Project title="Humans Vs. Goblins" description="Basic Game created in Java with GUI created with JavaFX" imgURL="images/HvG.png" link="https://github.com/jcaggese/GenSpark/tree/main/HumansVsGoblinsGUI"/>
+                    <Project title="Boarding Pass" description="Boarding Pass Form created in Java with JavaFX" imgURL="images/BoardingPass.png" link="https://github.com/jcaggese/Boarding-Pass"/>
+                    <Project title="Hangman Game" description="Hangman Game created using functional methods in Java" imgURL="images/Hangman.png" link="https://github.com/jcaggese/GenSpark/tree/main/HangmanFunctional"/>
                 </div>
                 <div className="row">
                     <Project title="RSA Cryptography Project" description="Java implementation of the RSA cryptographic algorithm" imgURL="images/EndBackground.jpg" link="https://github.com/WarrenDevonshire/CryptoProject3"/>
                     <Project title="Othello AI" description="Othello game created in Java with Swing. Uses an adversarial minimax algorithm" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/Othello-AI"/>
-                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
-                </div>
-                <div className="row">
-                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
-                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
-                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
                 </div>
             </div>
         </div>
