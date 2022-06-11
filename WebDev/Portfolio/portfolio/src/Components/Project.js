e267bb285cfaccfea383196623923a7f17df6b68
Jacob Caggese <jcaggese@gmail.com>
2022-06-10 21:01:31 -0400

    Project Styling Complete
    TODO: gather and add images for projects

--- WebDev/Portfolio/portfolio/src/Components/Project.js
+++ WebDev/Portfolio/portfolio/src/Components/Project.js
@@ -1,12 +1,13 @@
 import React from "react";
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-export default function Project({title, description, imgURL}) {
+export default function Project({title, description, imgURL, link}) {
     return (
         <div className="col project">
             <img src={imgURL}/>
             <h4>{title}</h4>
-            <p>{description}</p>
+            <p className="proj-desc">{description}</p>
+            <p><a href={link} className="proj-link">Github Link🔗</a></p>
         </div>
     )
 }
--- WebDev/Portfolio/portfolio/src/Components/ProjectTile.js
+++ WebDev/Portfolio/portfolio/src/Components/ProjectTile.js
@@ -8,19 +8,19 @@ export default function ProjectTile() {
             <h2>Projects</h2>
             <div className="container">
                 <div className="row">
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
+                    <Project title="Humans Vs. Goblins" description="Basic Game created in Java with GUI created with JavaFX" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/GenSpark/tree/main/HumansVsGoblinsGUI"/>
+                    <Project title="Boarding Pass" description="Boarding Pass Form created in Java with JavaFX" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/Boarding-Pass"/>
+                    <Project title="Hangman Game" description="Hangman Game created using functional methods in Java" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/GenSpark/tree/main/HangmanFunctional"/>
                 </div>
                 <div className="row">
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
+                    <Project title="RSA Cryptography Project" description="Java implementation of the RSA cryptographic algorithm" imgURL="images/EndBackground.jpg" link="https://github.com/WarrenDevonshire/CryptoProject3"/>
+                    <Project title="Othello AI" description="Othello game created in Java with Swing. Uses an adversarial minimax algorithm" imgURL="images/EndBackground.jpg" link="https://github.com/jcaggese/Othello-AI"/>
+                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
                 </div>
                 <div className="row">
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
-                    <Project title="Project" description="description" imgURL="images/Head.jpg"/>
+                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
+                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
+                    <Project title="Project" description="description" imgURL="images/EndBackground.jpg" link="/"/>
                 </div>
             </div>
         </div>
